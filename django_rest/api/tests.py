b725a739d62b926a22d61dd7c7f825b4b7c0c775
BarbaraBB <32848061+BarbaraBB@users.noreply.github.com>
2018-03-17 20:48:50 +0100

    tests.py for /login
    Testing login endpoint

--- django_rest/api/tests.py
+++ django_rest/api/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
+import json
+
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from rest_framework.test import APITestCase
 # Create your tests here.
+
+class LoginViewTestCase(APITestCase):
+    url = reverse("rest_login")
+
+    def setUp(self):
+        self.username = "test"
+        self.email = "test@test.com"
+        self.password = "testtest1"
+        self.user = User.objects.create_user(self.username, self.email, self.password)
+
+    def test_login_without_password(self):
+        response = self.client.post(self.url, {"email": self.email})
+        self.assertEqual(400, response.status_code)
+
+    def test_login_with_wrong_password(self):
+        response = self.client.post(self.url, {"email": self.email, "password": "testtest2"})
+        self.assertEqual(400, response.status_code)
+
+    def test_login_with_valid_data(self):
+        response = self.client.post(self.url, {"email": self.email, "password": self.password})
+        self.assertEqual(200, response.status_code)
+        self.assertTrue("key" in json.loads(response.content))
